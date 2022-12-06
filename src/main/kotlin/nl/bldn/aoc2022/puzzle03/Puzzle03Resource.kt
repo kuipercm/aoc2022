a7ad3ae143836e09cf6d25c649b5888ab4f6cf58
Niels Kuiper <kuiper.cm@gmail.com>
2022-12-06 14:04:01 +0100

    Puzzle03 Part 2

--- src/main/kotlin/nl/bldn/aoc2022/puzzle03/Puzzle03Resource.kt
+++ src/main/kotlin/nl/bldn/aoc2022/puzzle03/Puzzle03Resource.kt
@@ -13,17 +13,22 @@ class Puzzle03Resource {
         input: String,
     ): Int {
         val rucksacks = input.convertToPuzzleInput()
+        val groupsOfRuckSacks = rucksacks
+            .chunked(3)
+            .map { RuckSackGroup(it[0], it[1], it[2]) }
 
-        return rucksacks.map { it.findSingleCommon() }.sumOf { it.asPriority() }
+//        return rucksacks.map { it.findSingleCommon() }.sumOf { it.asPriority() }
         // Part 1: 8298
-        
+
+        return groupsOfRuckSacks.map { it.findSingleCommon() }.sumOf { it.asPriority() }
+        // Part 2:
     }
 
     private fun String.convertToPuzzleInput(): List<RuckSack> {
         return this.lines().map {
             val first = it.take(it.length / 2)
             val second = it.takeLast(it.length / 2)
-            RuckSack(first.toList(), second.toList())
+            RuckSack(it, first.toList(), second.toList())
         }
     }
 
@@ -39,10 +44,23 @@ class Puzzle03Resource {
 }
 
 private data class RuckSack(
+    val allContent: String,
     val front: List<Char>,
     val back: List<Char>,
 ) {
     fun findSingleCommon(): String {
         return front.intersect(back).single().toString()
     }
+}
+
+private data class RuckSackGroup(
+    val first: RuckSack,
+    val second: RuckSack,
+    val third: RuckSack,
+) {
+    fun findSingleCommon(): String =
+        first.allContent.toSet()
+            .intersect(second.allContent.toSet())
+            .intersect(third.allContent.toSet()).single()
+            .toString()
 }
